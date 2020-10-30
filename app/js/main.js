fbbdfb7f04c2bbac09347e442c0b0f2e56d449b5
SICKBOYRARI <jediz0rrr@gmail.com>
2020-10-30 21:01:17 +0700

    Added view all cast function

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,34 @@ const burgerOnClick = (openBtn, closeBtn, menu) => {
     onClick(burgerClose, 'none');
 }
 
+const viewAllCast = () => {
+    const cardsOpenBtn = document.querySelector('.card-open');
+    const cardsCloseBtn = document.querySelector('.card-close');
+    const cardBox = document.querySelector('.cast__content');
+    const cardPlug = document.querySelector('.card-plug');
+
+    function onClick(btn, displayFirst, displaySecond) {
+        btn.addEventListener('click', () => {
+            cardPlug.style.display = displayFirst;
+            for (let i = 6; i < 10; i++){
+                const cardBoxChildrens = cardBox.children;
+                cardBoxChildrens[i].style.display = displaySecond;
+                cardBoxChildrens[i + 1].style.marginTop = '40px';
+            }
+        })
+        // cardsCloseBtn.addEventListener('click', () => {
+        //     cardPlug.style.display = 'block';
+        //     for (let i = 6; i < 10; i++){
+        //         const cardBoxChildrens = cardBox.children;
+        //         cardBoxChildrens[i].style.display = 'none';
+        //         // cardBoxChildrens[i + 1].style.marginTop = '40px';
+        //     }
+        // })
+    }
+
+    onClick(cardsOpenBtn, 'none', 'block');
+    onClick(cardsCloseBtn, 'block', 'none');
+}
+
+viewAllCast();
 burgerOnClick('.navigation__item-open', '.burger__menu-btn', '.burger__menu');
